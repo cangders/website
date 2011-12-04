14e7d01ee67d5edfa0a3643482aeb43e2e2b002b
Charise Angderson <charise.angderson@gmail.com>
2011-12-04 17:40:39 -0600

    fixed link to images

--- js/lightbox.js
+++ js/lightbox.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'images/loading.gif',     
-    fileBottomNavCloseImage: 'images/closelabel.gif',
+    fileLoadingImage:        'img/loading.gif',     
+    fileBottomNavCloseImage: 'img/closelabel.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
